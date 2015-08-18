0167237b2bed45cc380d662b8013bcb21c5f33db
ReidasaurusRex <reidhlewis91@gmail.com>
2015-08-18 16:40:57 -0700

    Added relationships between expenditures and budgets

--- app/models/month_budget.rb
+++ app/models/month_budget.rb
@@ -1,3 +1,4 @@
 class MonthBudget < ActiveRecord::Base
   belongs_to :user
+  has_many :expenditures, as: :spendable
 end
--- app/models/week_budget.rb
+++ app/models/week_budget.rb
@@ -1,3 +1,4 @@
 class WeekBudget < ActiveRecord::Base
   belongs_to :user
+  has_many :expenditures, as: :spendable
 end
