ec48effd5e9a2e991e1ab83b33cbfebbbc58cead
ReidasaurusRex <reidhlewis91@gmail.com>
2015-08-22 11:28:31 -0700

    Began adding functionality to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
+  before_action :get_user, except: [:new, :create]
+
   def new
   end
 
   def create
+    create_user(user_params)
   end
 
   def show
+
   end
 
   def edit
@@ -19,4 +23,31 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:id, :username, :email, :password, :password_digest)
+  end
+
+  def create_user(new_user_params)
+    @user = User.new(new_user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_show_path(@user)
+    else
+      @user.destroy
+      flash[:alert] = @user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
+  end
+
+  def get_user
+    @user = User.find_by_id(params[:id])
+    if @user
+      return @user
+    # else
+    #   redirect to error page
+    end
+  end
+
 end
