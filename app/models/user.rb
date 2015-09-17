7f41ba685cbbe7e391e8663e373c7277319fb03f
ReidasaurusRex <reidhlewis91@gmail.com>
2015-09-16 20:05:11 -0700

    Added a function to automatically upcase the first letter of user's username

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ActiveRecord::Base
   has_one :month_budget
   has_many :expenditures, as: :spendable
   
+  def smart_username
+    username = self.username
+    if username.length > 1
+      return username[0].upcase + username[1..-1]
+    else
+      return username
+    end
+  end  
 end
