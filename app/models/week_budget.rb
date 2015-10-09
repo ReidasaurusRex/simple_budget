9dcf5f119cc8367fbd6f428a281fc498d2e6ef35
ReidasaurusRex <reidhlewis91@gmail.com>
2015-10-09 14:57:03 -0700

    Added functionality to check if a budget has expired.

--- app/models/week_budget.rb
+++ app/models/week_budget.rb
@@ -6,4 +6,13 @@ class WeekBudget < ActiveRecord::Base
   def expired_budget?
      Time.now - self.created_at > 7.days
   end
+
+  def self.proper_week_budget(user)
+    current_budget = user.week_budgets.last
+    if current_budget.expired_budget?
+      return self.create(target_amount: current_budget.target_amount, user_id: user.id)
+    else
+      return current_budget
+    end
+  end
 end
